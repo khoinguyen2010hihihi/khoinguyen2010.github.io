4222b5badaf717e25342b69153253df65d29ede0
Nguyen Khoi <khoibene@gmail.com>
2025-01-04 22:33:53 +0700

    submit

--- script.js
+++ script.js
@@ -5,29 +5,17 @@ const sendBtn = document.querySelector("#send-btn");
 const box = document.querySelector("#box");
 
 sendBtn.addEventListener("click", function(){
-  // const color = coInput.value;
-  // const height = heInput.value;
-  // const width = wiInput.value;
-
-  // if (color && height > 0 && width > 0) {
-  //   box.style.backgroundColor = color;
-  //   box.style.height = `${height}px`;
-  //   box.style.width = `${width}px`;
-  // } else {
-  //   alert("Vui lòng nhập đủ và chính xác tất cả các thông tin.");
-  // }
-
   const coOutput = coInput.value;
   const heOutput = heInput.value;
   const wiOutput = wiInput.value;
 
-  if((coOutput != null) && (heOutput != null) && (wiOutput != null)){
+  if(!coOutput || !heOutput || !wiOutput){
+    alert("Vui lòng nhập đủ và chính xác tất cả các thông tin.")
+  }
+  else{
     box.style.backgroundColor = `${coOutput}`;
     box.style.height = heOutput + "px";
     box.style.width = wiOutput + "px";
-  }
-  else{
-    alert("Vui lòng nhập đủ và chính xác tất cả các thông tin.")
   }
 }
 );
